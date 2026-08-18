f9ab16a56b2ee0971eacd23eeeb17299755e9f8a
Greg Brown <gk_brown@icloud.com>
2026-08-18 13:52:49 -0400

    Update MainFrame.

--- desktop/src/main/java/org/httprpc/helios/MainFrame.java
+++ desktop/src/main/java/org/httprpc/helios/MainFrame.java
@@ -1,13 +1,35 @@
 package org.httprpc.helios;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import org.httprpc.sierra.Outlet;
 import org.httprpc.sierra.UILoader;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import java.util.ResourceBundle;
 
 public class MainFrame extends JFrame implements Runnable {
+    private @Outlet JButton playButton = null;
+
+    private @Outlet JTextField searchTextField = null;
+
+    private @Outlet JSplitPane splitPane = null;
+
+    private @Outlet JScrollPane albumScrollPane = null;
+
+    private @Outlet JLabel libraryStatusLabel = null;
+
+    private FlatSVGIcon playIcon = new FlatSVGIcon(MainFrame.class.getResource("icons/play_arrow_24dp.svg"));
+    private FlatSVGIcon pauseIcon = new FlatSVGIcon(MainFrame.class.getResource("icons/pause_24dp.svg"));
+
     private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(MainFrame.class.getName());
 
     private MainFrame() {
@@ -20,12 +42,24 @@ public class MainFrame extends JFrame implements Runnable {
     public void run() {
         setContentPane(UILoader.load(this, "MainFrame.xml", resourceBundle));
 
-        pack();
+        // TODO
+        playButton.setIcon(playIcon);
+
+        // TODO Preferences
+        setSize(960, 640);
+
+        // TODO Preferences
+        splitPane.setDividerLocation(240);
+
+        // TODO
+        libraryStatusLabel.setText(" ");
+
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        FlatLightLaf.setup();
+        // TODO Preferences
+        FlatDarkLaf.setup();
 
         SwingUtilities.invokeLater(new MainFrame());
     }
