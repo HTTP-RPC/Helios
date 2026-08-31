b24e9fad62d30dd1634ae6ec8ad7ca08b5edcf41
Greg Brown <gk_brown@icloud.com>
2026-08-31 09:39:24 -0400

    Update UI.

--- desktop/src/main/java/org/httprpc/helios/AlbumDetailPanel.java
+++ desktop/src/main/java/org/httprpc/helios/AlbumDetailPanel.java
@@ -1,6 +1,6 @@
 package org.httprpc.helios;
 
-import org.httprpc.sierra.BasicListModel;
+import org.httprpc.sierra.ColumnPanel;
 import org.httprpc.sierra.ImagePane;
 import org.httprpc.sierra.Outlet;
 import org.httprpc.sierra.RowPanel;
@@ -8,67 +8,50 @@ import org.httprpc.sierra.StackPanel;
 import org.httprpc.sierra.UILoader;
 
 import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.ListCellRenderer;
 import javax.swing.UIManager;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
-import java.awt.Component;
 import java.time.Duration;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class AlbumDetailPanel extends StackPanel {
-    private static class SongCellRenderer extends RowPanel implements ListCellRenderer<Song> {
-        JLabel titleLabel = new JLabel();
-        JLabel timeLabel = new JLabel();
-
-        SongCellRenderer() {
-            setAlignToBaseline(true);
-
-            setBorder(new EmptyBorder(4, 0, 4, 0));
-
-            add(titleLabel, 1.0);
-            add(timeLabel);
-        }
-
-        @Override
-        public Component getListCellRendererComponent(JList<? extends Song> list,
-            Song song, int index,
-            boolean selected, boolean cellHasFocus) {
-            titleLabel.setText(song.getTitle());
-
-            var duration = Duration.ofSeconds(song.getTime());
-
-            timeLabel.setText(String.format(resourceBundle.getString("timeFormat"),
-                duration.toMinutesPart(),
-                duration.toSecondsPart()));
-
-            return this;
-        }
-    }
+    private @Outlet JLabel nameLabel = null;
 
     private @Outlet ImagePane imagePane = null;
-
-    private @Outlet JLabel nameLabel = null;
-    private @Outlet JList<Song> songList = null;
+    private @Outlet ColumnPanel songListPanel = null;
 
     private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(AlbumDetailPanel.class.getName());
 
     public AlbumDetailPanel(Artist artist, String name, List<Song> songs) {
         add(UILoader.load(this, "AlbumDetailPanel.xml"));
 
-        imagePane.setImage(MainFrame.getAlbumArtwork(artist, name));
-
         nameLabel.setText(name);
 
-        songList.setCellRenderer(new SongCellRenderer());
-        songList.setModel(new BasicListModel<>(songs));
+        imagePane.setImage(MainFrame.getAlbumArtwork(artist, name));
 
-        songList.setBorder(new CompoundBorder(
+        songListPanel.setBorder(new CompoundBorder(
             new MatteBorder(1, 0, 0, 0, UIManager.getColor("Component.borderColor")),
             new EmptyBorder(2, 0, 0, 0)
         ));
+
+        for (var song : songs) {
+            var songPanel = new RowPanel();
+
+            songPanel.setAlignToBaseline(true);
+
+            songPanel.add(new JLabel(song.getTitle()), 1.0);
+
+            var duration = Duration.ofSeconds(song.getTime());
+
+            songPanel.add(new JLabel(String.format(resourceBundle.getString("timeFormat"),
+                duration.toMinutesPart(),
+                duration.toSecondsPart())));
+
+            songPanel.setBorder(new EmptyBorder(4, 0, 4, 0));
+
+            songListPanel.add(songPanel);
+        }
     }
 }
