29dbec9af297687695b5e187c48ef2d24cb0def7
Greg Brown <gk_brown@icloud.com>
2026-09-02 18:34:17 -0400

    Update UI.

--- desktop/src/main/java/org/httprpc/helios/Song.java
+++ desktop/src/main/java/org/httprpc/helios/Song.java
@@ -1,7 +1,6 @@
 package org.httprpc.helios;
 
 import org.httprpc.kilo.Name;
-import org.httprpc.kilo.Required;
 import org.httprpc.kilo.sql.Column;
 import org.httprpc.kilo.sql.ForeignKey;
 import org.httprpc.kilo.sql.Index;
@@ -19,24 +18,20 @@ public interface Song {
     @Column("artist")
     @ForeignKey(Artist.class)
     @Index(1)
-    @Required
     String getArtist();
     void setArtist(String artist);
 
     @Column("album")
     @Index(2)
-    @Required
     String getAlbum();
     void setAlbum(String album);
 
     @Column("title")
     @Index(3)
-    @Required
     String getTitle();
     void setTitle(String title);
 
     @Column("time")
-    @Required
     Integer getTime();
     void setTime(Integer time);
 
