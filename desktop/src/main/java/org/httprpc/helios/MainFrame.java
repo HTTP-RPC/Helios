e9c5d269d92083769e4976dc3cc3b48b16fe6fc0
Greg Brown <gk_brown@icloud.com>
2026-09-05 10:45:24 -0400

    Update UI.

--- desktop/src/main/java/org/httprpc/helios/MainFrame.java
+++ desktop/src/main/java/org/httprpc/helios/MainFrame.java
@@ -175,11 +175,9 @@ public class MainFrame extends JFrame implements Runnable {
     private @Outlet JTabbedPane collectionTabbedPane = null;
 
     private @Outlet JScrollPane artistScrollPane = null;
-    private @Outlet JLabel noArtistsLabel = null;
     private @Outlet JList<ExpandedArtist> artistList = null;
 
     private @Outlet JScrollPane playlistScrollPane = null;
-    private @Outlet JLabel noPlaylistsLabel = null;
     private @Outlet JList<ExpandedPlaylist> playlistList = null;
 
     private @Outlet JScrollPane collectionScrollPane = null;
@@ -481,10 +479,6 @@ public class MainFrame extends JFrame implements Runnable {
             } else {
                 artistList.setSelectedIndex(0);
             }
-
-            noArtistsLabel.setVisible(false);
-        } else {
-            noArtistsLabel.setVisible(true);
         }
     }
 
@@ -509,10 +503,6 @@ public class MainFrame extends JFrame implements Runnable {
             } else {
                 playlistList.setSelectedIndex(0);
             }
-
-            noPlaylistsLabel.setVisible(false);
-        } else {
-            noPlaylistsLabel.setVisible(true);
         }
     }
 
