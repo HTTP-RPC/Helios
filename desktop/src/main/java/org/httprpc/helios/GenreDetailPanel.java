360cbd2ea015da42c975034b2671deb38b7061e6
Greg Brown <gk_brown@icloud.com>
2026-09-07 10:46:38 -0400

    Update UI.

--- desktop/src/main/java/org/httprpc/helios/GenreDetailPanel.java
+++ desktop/src/main/java/org/httprpc/helios/GenreDetailPanel.java
@@ -2,21 +2,62 @@
 
 package org.httprpc.helios;
 
+import org.httprpc.sierra.BasicTableModel;
 import org.httprpc.sierra.Outlet;
 import org.httprpc.sierra.StackPanel;
 import org.httprpc.sierra.UILoader;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.TableCellRenderer;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.KeyboardFocusManager;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import static org.httprpc.kilo.util.Collections.*;
+import static org.httprpc.kilo.util.Optionals.*;
+
 public class GenreDetailPanel extends StackPanel {
+    private static class GenreCellRenderer extends JLabel implements TableCellRenderer {
+        GenreCellRenderer() {
+            setText("A");
+            setOpaque(true);
+
+            setBorder(new EmptyBorder(4, 4, 4, 4));
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table,
+            Object value,
+            boolean selected, boolean hasFocus,
+            int row, int column) {
+            setText(map(value, Object::toString));
+
+            Color background;
+            Color foreground;
+            if (selected) {
+                background = table.getSelectionBackground();
+                foreground = table.getSelectionForeground();
+            } else {
+                background = table.getBackground();
+                foreground = table.getForeground();
+            }
+
+            setBackground(background);
+            setForeground(foreground);
+
+            return this;
+        }
+    }
+
     private @Outlet JLabel nameLabel = null;
     private @Outlet JButton playAllButton = null;
 
-    // TODO
+    private @Outlet JTable songTable = null;
 
     private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(GenreDetailPanel.class.getName());
 
@@ -25,12 +66,28 @@ public class GenreDetailPanel extends StackPanel {
 
         nameLabel.setText(genre.getName());
 
-        // TODO Play all button
+        playAllButton.addActionListener(event -> MainFrame.getInstance().playAll(songs));
 
-        // TODO Album list
+        var songTableHeader = songTable.getTableHeader();
+
+        songTableHeader.setReorderingAllowed(false);
+        songTableHeader.setResizingAllowed(false);
+
+        songTable.setFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, null);
+        songTable.setFocusTraversalKeys(KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS, null);
+
+        songTable.setModel(new BasicTableModel<>(Song.class, songs,
+            listOf("title", "artist", "album"),
+            resourceBundle));
+
+        var genreCellRenderer = new GenreCellRenderer();
+
+        songTable.setDefaultRenderer(Object.class, genreCellRenderer);
+        songTable.setRowHeight(genreCellRenderer.getPreferredSize().height);
 
         setBorder(new EmptyBorder(8, 8, 8, 8));
 
         setScrollableTracksViewportWidth(true);
+        setScrollableTracksViewportHeight(true);
     }
 }
