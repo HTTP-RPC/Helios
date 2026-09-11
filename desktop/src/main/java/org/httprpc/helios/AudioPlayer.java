a0dc09cb63f90ef26daa26827c920fdd4afd0a7c
Greg Brown <gk_brown@icloud.com>
2026-09-11 14:30:06 -0400

    Update UI.

--- desktop/src/main/java/org/httprpc/helios/AudioPlayer.java
+++ desktop/src/main/java/org/httprpc/helios/AudioPlayer.java
@@ -4,6 +4,7 @@ package org.httprpc.helios;
 
 import com.sun.jna.Platform;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public interface AudioPlayer {
@@ -15,12 +16,36 @@ public interface AudioPlayer {
     void dispose();
 
     static AudioPlayer create(Path contentPath) {
-        if (Platform.isMac()) {
-            return new MacOSAudioPlayer(contentPath);
-        } else if (Platform.isWindows()) {
-            return new WindowsAudioPlayer(contentPath);
+        if (Files.exists(contentPath)) {
+            if (Platform.isMac()) {
+                return new MacOSAudioPlayer(contentPath);
+            } else if (Platform.isWindows()) {
+                return new WindowsAudioPlayer(contentPath);
+            } else {
+                throw new UnsupportedOperationException();
+            }
         } else {
-            throw new UnsupportedOperationException();
+            return new AudioPlayer() {
+                @Override
+                public void play() {
+                    // No-op
+                }
+
+                @Override
+                public void pause() {
+                    // No-op
+                }
+
+                @Override
+                public boolean isPlaying() {
+                    return false;
+                }
+
+                @Override
+                public void dispose() {
+                    // No-op
+                }
+            };
         }
     }
 }
