77da1cc1936a12c52e3937774d8570a6be831496
Greg Brown <gk_brown@icloud.com>
2026-09-25 17:40:20 -0400

    Update build.

--- src/main/cpp/org_httprpc_helios_WindowsAudioPlayer.cpp
+++ src/main/cpp/org_httprpc_helios_WindowsAudioPlayer.cpp
@@ -1,4 +1,51 @@
 #include <jni.h>
 #include "org_httprpc_helios_WindowsAudioPlayer.h"
 
-// TODO
+JNIEXPORT jlong JNICALL Java_org_httprpc_helios_WindowsAudioPlayer_construct
+  (JNIEnv *, jobject, jstring) {
+    // TODO
+    return 0;
+}
+
+JNIEXPORT void JNICALL Java_org_httprpc_helios_WindowsAudioPlayer_play
+  (JNIEnv *, jobject, jlong) {
+    // TODO
+}
+
+JNIEXPORT void JNICALL Java_org_httprpc_helios_WindowsAudioPlayer_pause
+  (JNIEnv *, jobject, jlong) {
+    // TODO
+}
+
+JNIEXPORT jboolean JNICALL Java_org_httprpc_helios_WindowsAudioPlayer_isPlaying
+  (JNIEnv *, jobject, jlong) {
+    // TODO
+    return 0;
+}
+
+JNIEXPORT jdouble JNICALL Java_org_httprpc_helios_WindowsAudioPlayer_getPosition
+  (JNIEnv *, jobject, jlong) {
+    // TODO
+    return 0;
+}
+
+JNIEXPORT void JNICALL Java_org_httprpc_helios_WindowsAudioPlayer_setPosition
+  (JNIEnv *, jobject, jdouble, jdouble) {
+    // TODO
+}
+
+JNIEXPORT jdouble JNICALL Java_org_httprpc_helios_WindowsAudioPlayer_getVolume
+  (JNIEnv *, jobject, jlong) {
+    // TODO
+    return 0;
+}
+
+JNIEXPORT void JNICALL Java_org_httprpc_helios_WindowsAudioPlayer_setVolume
+  (JNIEnv *, jobject, jlong, jdouble) {
+    // TODO
+}
+
+JNIEXPORT void JNICALL Java_org_httprpc_helios_WindowsAudioPlayer_dispose
+  (JNIEnv *, jobject, jlong) {
+    // TODO
+}
